f4803208910b97424e303389b13eee10c88787a8
Isabella <isa021andrade@gmail.com>
2025-07-10 22:19:52 -0300

    feat(models): add removeTodo method into Project class

--- src/Project.js
+++ src/Project.js
@@ -1,10 +1,22 @@
 export class Project {
-    construtor(name){
+    constructor(name) {
         this.id = this.generateId();
         this.name = name;
+        this.todos = [];
     }
 
     generateId() {
         return crypto.randomUUID();
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo);
+    }
+
+    removeTodo(id) {
+        const index = this.todos.findIndex((todo) => {
+            return todo.id === id;
+        });
+        this.todos.splice(index, 1)
     }
 }
